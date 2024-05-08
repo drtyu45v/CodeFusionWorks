bade34e95569415ff14a1d0b768c52eb52cbb089
drtyu45v <jsjcasonkyle728@hotmail.com>
2024-05-08 21:18:32 +0700

    hello commit

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function findMaxLength(nums) {
-  const map = new Map();
-  map.set(0, -1);
-  let count = 0;
-  let maxLength = 0;
-  for (let i = 0; i < nums.length; i++) {
-    count += nums[i] === 1 ? 1 : -1;
-    if (map.has(count)) {
-      maxLength = Math.max(maxLength, i - map.get(count));
-    } else {
-      map.set(count, i);
-    }
-  }
-  return maxLength;
+function maxDepth(root) {
+  if (!root) return 0;
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 }
