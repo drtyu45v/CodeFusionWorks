71bc82ee5404f780cd6a3f0429e5dda0e169e59b
drtyu45v <jsjcasonkyle728@hotmail.com>
2024-05-11 20:02:05 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-function maxSlidingWindow(nums, k) {
-  const result = [];
-  const queue = [];
-  for (let i = 0; i < nums.length; i++) {
-    while (queue.length && nums[i] >= nums[queue[queue.length - 1]]) {
-      queue.pop();
-    }
-    queue.push(i);
-    if (queue[0] === i - k) queue.shift();
-    if (i >= k - 1) result.push(nums[queue[0]]);
-  }
-  return result;
+const fruits = ["apple", "banana", "orange"];
+for (const fruit of fruits) {
+  console.log(fruit);
 }
